2295fbe991a68cc914099c786abfa287dcdf1609
Toivo Henningsson <toivo@control.lth.se>
2012-12-14 13:21:47 +0100

    Reorganize to use as a basis for Base.Meta, add some tests

--- src/Toivo.jl
+++ src/Toivo.jl
@@ -1,7 +1,70 @@
 
 module Toivo
 using Base 
-export @show, @expect, quot, is_expr, split_fdef, @in, @sexpr, show_sexpr
+export quot, is_expr, @sexpr, show_sexpr
+export @expect, split_fdef, @in
+
+
+# ---- Metaprogramming --------------------------------------------------------
+
+quot(ex) = expr(:quote, ex)
+
+is_expr(ex::Expr, head)          = ex.head === head
+is_expr(ex::Expr, heads::Set)    = has(heads, ex.head)
+is_expr(ex::Expr, heads::Vector) = contains(heads, ex.head)
+is_expr(ex,       head)          = false
+is_expr(ex,       head, n::Int)  = is_expr(ex, head) && length(ex.args) == n
+
+
+# ---- show_sexpr: print an AST as an S-expression ----
+
+show_sexpr(ex) = show_sexpr(OUTPUT_STREAM, ex)
+show_sexpr(io::IO, ex) = (print(io, "@sexpr "); show_sexpr_raw(io, ex))
+
+show_sexpr_raw(io::IO, ex) = show_sexpr_raw(io, ex, 0)
+show_sexpr_raw(io::IO, ex, indent::Int) = show(io, ex)
+
+const sexpr_indent_width = 2
+
+function show_sexpr_raw(io::IO, ex::QuoteNode, indent::Int)
+    inner = indent + sexpr_indent_width
+    print(io, "(:quote, #QuoteNode\n", " "^inner)
+    show_sexpr_raw(io, ex.value, inner)
+    print(io, '\n', " "^indent, ')')
+end
+function show_sexpr_raw(io::IO, ex::Expr, indent::Int)
+    inner = indent + sexpr_indent_width
+    print(io, '(')
+    show_sexpr_raw(io, ex.head, inner)
+    for arg in ex.args
+        print(io, ex.head === :block ? ",\n"*" "^inner : ", ")
+        show_sexpr_raw(io, arg, inner)
+    end
+    if length(ex.args) == 0; print(io, ",)")
+    else print(io, (ex.head === :block ? "\n"*" "^indent : ""), ')')
+    end
+end
+
+# ---- @sexpr: make an AST from an S-expression ----
+
+macro sexpr(ex)
+    esc(sexpr_to_expr(ex))
+end
+
+sexpr_to_expr(ex) = quot(ex)
+sexpr_to_expr(ex::QuoteNode) = ex
+function sexpr_to_expr(ex::Expr)
+    head, args = ex.head, ex.args
+    if head === :tuple
+        h = :( $(sexpr_to_expr(args[1]))::Symbol )
+        expr(:call, :expr, h, {sexpr_to_expr(arg) for arg in args[2:end]}...)
+    elseif head === :quote; ex
+    else expr(:quote, ex)
+    end
+end
+
+
+# ---- More stuff -------------------------------------------------------------
 
 macro expect(pred)
     quote
@@ -9,11 +72,6 @@ macro expect(pred)
           $(string("expected: ", sprint(Base.show_unquoted, pred)", == true")))
     end
 end
-
-quot(ex) = expr(:quote, ex)
-
-is_expr(ex, head::Symbol) = (isa(ex, Expr) && (ex.head == head))
-is_expr(ex, head::Symbol, n::Int) = is_expr(ex, head) && length(ex.args) == n
 
 # Return the signature and body from a named method definition, either syntax.
 # E.g. split_fdef( :( f(x) = x^2) ) == (:(f(x)), :(x^2))
@@ -30,56 +88,6 @@ split_fdef(f::Any) = error("split_fdef: expected function definition, got\n$f")
 macro in(mod, ex)
     quote
         $mod.eval($(expr(:quote,ex)))
-    end
-end
-
-macro sexpr(ex)
-    esc(sexpr_to_expr(ex))
-end
-
-sexpr_to_expr(ex) = expr(:quote, ex)
-sexpr_to_expr(ex::QuoteNode) = ex
-function sexpr_to_expr(ex::Expr)
-    head, args = ex.head, ex.args
-    if head === :tuple
-        h = sexpr_to_head(args[1])
-        expr(:call, :expr, h, {sexpr_to_expr(arg) for arg in args[2:end]}...)
-    elseif head === :quote; ex
-    else expr(:quote, ex)
-    end
-end
-
-sexpr_to_head(ex::Symbol) = expr(:quote, ex)
-sexpr_to_head(ex::QuoteNode) = ex
-function sexpr_to_head(ex)
-    if is_expr(ex, :quote, 1); ex
-    else error("@sexpr: Cannot interpret $ex as a head")
-    end
-end
-
-
-# ---- show_sexpr: print an AST as an S-expression ----
-
-show_sexpr(ex) = show_sexpr(OUTPUT_STREAM, ex)
-show_sexpr(io::IO, ex) = show_sexpr(io, ex, 0)
-show_sexpr(io::IO, ex, indent::Int) = show(io, ex)
-
-const sexpr_indent_width = 2
-
-function show_sexpr(io::IO, ex::Expr, indent::Int)
-    inner = indent + sexpr_indent_width
-    if (ex.head === :block) inter, post = (",\n"*" "^inner, "\n"*" "^indent)
-    else inter, post = (", ", "")
-    end
-
-    print(io, '(')
-    show_sexpr(io, ex.head, inner)
-    for arg in ex.args
-        print(io, inter)
-        show_sexpr(io, arg, inner)
-    end
-    if length(ex.args) == 0; print(io, ",)")
-    else print(io, post, ')')
     end
 end
 
